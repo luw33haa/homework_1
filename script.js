7a986c5caa5e260e68420218664f7c08c1d44def
luw33haa <120402146+luw33haa@users.noreply.github.com>
2022-12-20 21:17:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-const student = "Зайротьянц Захар Денисович"; 
-
+const student = "Зайротьянц Захар Денисович"; // Очевидно, что здесь ваши личные Фамилия, Имя и Отчество
 document.getElementById("student").innerHTML = student;
- 
-let n1 = prompt('в Введите предполагаемый результат первой команды')
-let n2 = prompt('в Введите предполагаемый результат второй команды')
-let n3 = prompt('в Введите реальный результат первой команды')
-let n4 = prompt('в Введите реальный результат второй команды')   
-let score_real = [n3,n4];
-let score_expected = [n1,n2];
-if  
-(n1>n2) {score_real=0} 
-else if 
-(n1==n2) {score_real=1}
-else
- score_real=2
-if  
-(n3>n4) {score_expected=0} 
-else if
- (n3==n4) {score_expected=1}
-else 
-score_expected=2 
-if (n2==n4,n1==n3) 
- {
-    alert( 'Счёт и результат были угаданы верно !!' );
-   }  
-else if (score_real==score_expected)
-{
-  alert( 'Счёт не угадали,!' )
-    alert( 'результат был угадан верно!!' );
-}
-else
-{ 
-  alert( 'Счёт не угадали, результат не был угадан!' )
-
-}
+let Jamaica = prompt('Введите реальный счет первой команды'); 
+let Argentina = prompt('Введите реальный счет второй команды'); 
+let Jamaica0 = prompt('Введие предполагаемый счет первой команды'); 
+let Argentina5 = prompt('Введите предполагаемый счет второй команды'); 
+if ((Jamaica==Jamaica0)&&(Argentina==Argentina5))
+{alert('Счет был угадан верно!')}
+else if (((Jamaica==Argentina)&&(Jamaica0==Argentina5)) || ((Jamaica>Argentina)&&(Jamaica0>Argentina5)) || ((Jamaica<Argentina)&&(Jamaica0<Argentina5))) {
+alert('Счет не угадали, но угадали исход!')}
+else {alert('Ни счета, ни исхода не угадали!')}
